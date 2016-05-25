fa63f924e4b57cda22c9f738446554c478111975
Awei <awei0905@gmail.com>
2016-05-25 23:24:19 +0800

    Change char array to char variable

--- TextFileEncryptDecrypt/TextFileEncryptDecrypt/Source.cpp
+++ TextFileEncryptDecrypt/TextFileEncryptDecrypt/Source.cpp
@@ -1,49 +1,60 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
-#define SIZE 4096
 
 int main()
 {
-	FILE* file = fopen("plane.txt", "r");
-	char buf[SIZE] = { 0 };
-	int i;
-	if (file) {
-		i = 0;
-		while ((buf[i] = fgetc(file)) != EOF)
+	FILE *fileIn, *fileOut;
+	char p, c;
+
+	// 開啟檔案
+	if (!(fileIn = fopen("plane.txt", "r"))) { printf("無法開啟plane.txt\n"); system("pause"); return 0; } // 輸入
+	if (!(fileOut = fopen("cypher.txt", "w"))) { printf("無法開啟cypher.txt\n"); system("pause"); return 0; } // 輸出
+
+	// 從明文文字檔案一個字元一個字元讀取
+	while ((p = fgetc(fileIn)) != EOF)
+	{
+		printf("%c", p); // 顯示讀取到的字元
+
+		// 輸出一個字元一個字元到密文文字檔案
+		if (p == 13 || p == 10)
 		{
-			printf("%c", buf[i]);
-			if (buf[i] == 13 || buf[i] == 10)
-			{
-				i++;
-				continue;
-			}
-			buf[i++] = ((113 * (buf[i] - 32) + 45) % 94) + 32;
+			fputc(p, fileOut);
+			continue;
 		}
-		printf("\n");
-		fclose(file);
-		file = fopen("cypher.txt", "w");
-		fwrite(buf, sizeof(char), i, file);
-		fclose(file);
-		file = fopen("cypher.txt", "r");
-		i = 0;
-		while (((buf[i] = fgetc(file)) != EOF))
-		{
-			printf("%c", buf[i]);
-			if (buf[i] == 13 || buf[i] == 10)
-			{
-				i++;
-				continue;
-			}
-			buf[i++] = ((5 * (buf[i] - 32) + 57) % 94) + 32;
-		}
-		fclose(file);
-		file = fopen("decry.txt", "w");
-		fwrite(buf, sizeof(char), i, file);
-		fclose(file);
+		c = ((113 * (p - 32) + 45) % 94) + 32; // 加密公式
+		fputc(c, fileOut);
 	}
-	else printf("無法開啟plane.txt\n");
 	printf("\n");
+
+	// 關閉已開啟的檔案
+	fclose(fileIn);
+	fclose(fileOut);
+
+	// 開啟檔案
+	if (!(fileIn = fopen("cypher.txt", "r"))) { printf("無法開啟cypher.txt\n"); system("pause"); return 0; } // 輸入
+	if (!(fileOut = fopen("decry.txt", "w"))) { printf("無法開啟decry.txt\n"); system("pause"); return 0; } // 輸出
+	
+	// 從密文文字檔案一個字元一個字元讀取
+	while ((c = fgetc(fileIn)) != EOF)
+	{
+		printf("%c", c); // 顯示讀取到的字元
+
+		// 輸出一個字元一個字元到明文文字檔案
+		if (c == 13 || c == 10)
+		{
+			fputc(c, fileOut);
+			continue;
+		}
+		p = ((5 * (c - 32) + 57) % 94) + 32; // 解密公式
+		fputc(p, fileOut);
+	}
+	printf("\n");
+
+	// 關閉已開啟的檔案
+	fclose(fileIn);
+	fclose(fileOut);
+
 	system("pause");
 	return 0;
 }
